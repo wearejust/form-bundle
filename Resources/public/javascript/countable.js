0140c772ec6685f694f5b6633edca81e61335dd0
Dylan Hoogenboom <dylan.hoogenboom@wearejust.com>
2017-09-28 10:18:37 +0200

    Chore: changes some classes arround to make it more 1 module

--- Resources/public/javascript/countable.js
+++ Resources/public/javascript/countable.js
@@ -5,20 +5,20 @@ $(function(){
       let counter_attr = count_area.attr('data-length');
       let total = counter_attr - count_area.val().length;
 
-      let count_text =  item.find('.total_text');
+      let count_text =  item.find('.js-countable-total');
       count_text.text(total);
       if(isNaN(total) || total <= 0) {
-        count_text.addClass('error');
+        count_text.addClass('c-counter-error');
       }else {
-        count_text.removeClass('error');
+        count_text.removeClass('c-counter-error');
       }
       count_area.on('keyup', function(){
             let total = counter_attr - count_area.val().length;
             count_text.text(total);
             if(isNaN(total) || total <= 0) {
-              count_text.addClass('error');
+              count_text.addClass('c-counter-error');
             }else {
-              count_text.removeClass('error');
+              count_text.removeClass('c-counter-error');
             }
       })
   });
